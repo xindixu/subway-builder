04b2490c9959e3de0e927a07044df5c6e3cbe946
Xindi Xu <xindixu@utexas.edu>
2019-03-26 21:42:38 -0500

    refactor folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Layout from './components/Layout/Layout'
+import Layout from './hoc/Layout/Layout'
 import SubwayBuilder from './containers/SubwayBuilder/SubwayBuilder'
 
 
